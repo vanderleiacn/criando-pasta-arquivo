305b7bc3d8da34d26daad70ada10021c19d8de0e
vanderleiacn <vanderleiacn@gmail.com>
2023-04-06 15:56:59 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace CriandoUmaPasta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOk = new System.Windows.Forms.Button();
+            this.dt = new System.Windows.Forms.DateTimePicker();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(138, 251);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(97, 46);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // dt
+            // 
+            this.dt.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dt.Location = new System.Drawing.Point(98, 210);
+            this.dt.Name = "dt";
+            this.dt.Size = new System.Drawing.Size(184, 26);
+            this.dt.TabIndex = 1;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(98, 153);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(184, 26);
+            this.txtNome.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.dt);
+            this.Controls.Add(this.btnOk);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.DateTimePicker dt;
+        private System.Windows.Forms.TextBox txtNome;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CriandoUmaPasta      // criando uma pasta e um arquivo dentro da pasta 
+{
+    public partial class Form1 : Form
+    {   //variavel
+        string caminhoArquivo = @"C:\configuracao\"; //nome da pasta 
+        string nomeArquivo = @"config.data"; //nome do arquivo 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            //criar pastas
+            if (!Directory.Exists(caminhoArquivo))
+            {
+                Directory.CreateDirectory(caminhoArquivo);// cria
+                // StreamWriter grava
+                StreamWriter arquivo = new StreamWriter(caminhoArquivo + nomeArquivo, false,Encoding.Default); //arquivo é o nome da instancia
+                arquivo.WriteLine(txtNome.Text); 
+                arquivo.WriteLine(dt.Value.ToShortDateString());
+
+                MessageBox.Show("Criado com sucesso");
+            }
+            else
+            {
+                MessageBox.Show("Diretório existente");
+            }
+            Application.Exit();
+        }
+    }
+}
